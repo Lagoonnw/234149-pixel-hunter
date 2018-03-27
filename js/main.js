74379771b10a452fe32002b280b9d944d7ff6803
Ekaterina K <lagoonnw@gmail.com>
2018-03-27 18:11:09 +0500

    Убирает отдельные ссылки на темплэйты и добавляет коллекцию

--- js/main.js
+++ js/main.js
@@ -1,11 +1,5 @@
 const mainScreen = document.querySelector(`main.central`);
-const greetingScreen = document.querySelector(`#greeting`).content;
-const rulesScreen = document.querySelector(`#rules`).content;
-const statsScreen = document.querySelector(`#stats`).content;
-const game1Screen = document.querySelector(`#game-1`).content;
-const game2Screen = document.querySelector(`#game-2`).content;
-const game3Screen = document.querySelector(`#game-3`).content;
-const screens = [greetingScreen, rulesScreen, game1Screen, game2Screen, game3Screen, statsScreen];
+const screens = document.querySelectorAll(`template`);
 const keyCodes = {
   ARROW_RIGHT: 39,
   ARROW_LEFT: 37
@@ -13,7 +7,7 @@ const keyCodes = {
 let currentScreenNumber = 0;
 
 const renderScreen = (number) => {
-  let node = screens[number].cloneNode(true);
+  let node = screens[number].content.cloneNode(true);
 
   while (mainScreen.firstChild) {
     mainScreen.removeChild(mainScreen.firstChild);
