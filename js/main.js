f3b5bb87f1b69e094e6a674c5c5c392c99826a85
Ekaterina K <lagoonnw@gmail.com>
2018-04-04 20:27:02 +0500

    Правит main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 import introScreen from './screens/intro.js';
 import renderScreen from './utils/render-screen.js';
-// import Timer from './data/timer.js';
-// const timer = new Timer(-2);
+
 renderScreen(introScreen);
-// console.log(timer);
+
 
